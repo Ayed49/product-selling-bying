8f874531c81bf8d2fedcdf8084472bf671117a11
Ayed49 <ayedbabu60@gmail.com>
2024-02-28 15:22:33 +0600

    first commit

--- pin-matcher-master/index.js
+++ pin-matcher-master/index.js
@@ -1,25 +1,32 @@
-function getPin(){
+function getPin() {
     const pin = Math.round(Math.random() * 10000);
     const pinString = pin + '';
-    if(pinString.length == 4){
+    if (pinString.length == 4) {
         return pin;
     }
-    else{     
+    else {
+        console.log('got 3 digit', pin);
         return getPin();
     }
 }
-function generatePin(){
+function generatePin() {
     const pin = getPin();
-    document.getElementById('display-pin').value=pin;
-    
+    document.getElementById('display-pin').value = pin;
 }
-
-// calculator
 document.getElementById('key-pad').addEventListener('click', function (event) {
-  const number = event.target.innerText;
-  const calcInput = document.getElementById('typed-numbers');
-  const previousNumber = calcInput.value;
-  const newNumber = previousNumber + number;
-  calcInput.value = newNumber;
-  
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+
+    // console.log(number);
 })
